447be743df69dfb3c1e2809109b14ba4b6092a34
Mike Blackstock <blackstock.mike@gmail.com>
2020-03-06 09:12:01 -0500

    Update config.js

--- src/client/config.js
+++ src/client/config.js
@@ -34,11 +34,108 @@
 // Complete config tree: https://github.com/os-js/osjs-client/blob/master/src/config.js
 //
 
-export default {
+/*!
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) 2011-2019, Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+//
+// This is the client configuration tree.
+// Guide: https://manual.os-js.org/v3/config/#client
+// Complete config tree: https://github.com/os-js/osjs-client/blob/master/src/config.js
+//
+
+module.exports = {
+	ws: {
+		uri:'/ws'
+	},
+
   auth: {
     login: {
       username: 'demo',
       password: 'demo'
     }
   }
+
+/*
+auth: {
+    ui: {
+		title: '',
+      fields: [ {
+          tagName: 'p',
+          attributes: {
+            style: 'font-size:x-large; text-align: center; color: white'
+          },
+            children: 'OMET.CA'
+        },
+		{
+          tagName: 'p',
+          attributes: {
+            style: 'font-weight:bold; font-size:x-large; text-align: center; color: white'
+          },
+            children: 'Online Music Engraving Tools'
+        },
+
+          {
+        tagName: 'input',
+        attributes: {
+          name: 'username',
+          type: 'text',
+          value: 'demo',
+          placeholder: 'Username'
+        }
+      }, {
+        tagName: 'input',
+        attributes: {
+          name: 'password',
+          type: 'password',
+          value:'demo',
+          placeholder: 'Password'
+        }
+
+      }, {
+        tagName: 'input',
+        attributes: {
+          type: 'submit',
+          value: 'Login'
+        }
+      }
+//      {
+//      tagName: 'a',
+//        attributes: {
+//          href: 'http://register.omet.ca'
+//        },
+//        children: 'No account? Register here'
+//      }
+      ]
+    }
+  }
+ 
+ */
+
 };
