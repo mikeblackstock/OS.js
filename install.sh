6235fab4d3b53a275410939456f756142aaf4884
Mikey Blackey <mike@omet.ca>
2020-05-18 21:06:44 -0400

    install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#npm install
+#npm run build
+#npm run package:discover
+ln -s  ../node_modules/dojo dist/dojo
+ln -s  ../node_modules/dijit dist/dijit
+ln -s  ../node_modules/codemirror dist/codemirror
+echo   "--- OMET installed  ---"
+
